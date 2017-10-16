2c5240866b267b7f177b5550f2ad0ed134bb02ba
Connor Gallopo <connor.gallopo@me.com>
2017-10-16 02:09:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@ def turn_count(board)
   counter = 0
   board.each do |turn|
     if turn != " " || turn != "" || turn != nil
-      counter += 1
       return counter
+      counter += 1
     end
   end
 end
